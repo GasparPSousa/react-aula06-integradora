b18b585cf409820b8895c2fb416761971cfa4214
GasparPSousa <gasparufrj@gmail.com>
2021-11-03 12:01:25 -0300

    Mesa07 - Usando o Map para alterar a mesa06

--- src/container/App.js
+++ src/container/App.js
@@ -3,16 +3,35 @@ import ClassComponent from '../components/ClassComponent'
 import FunctionComponent  from '../components/FunctionComponent';
 
 function App() {
+
+  const listaConvidados = [ 
+    { nome:'Nicolas', estaNaLista:true, tarefa:'batata-frita' }, 
+    { nome:'Pedro', estaNaLista:false, tarefa:'pizza' }, 
+    { nome:'Carolina', estaNaLista:true, tarefa:'bebidas' } 
+  ] 
+
   return (
     <div className="App">
+
       <h3>Convidados: </h3>
-      <ClassComponent nome="Nicolas" estaNaLista={true} />
+
+      { listaConvidados.map(({nome, estaNaLista, tarefa}) => {
+        return (
+          <>
+            <ClassComponent nome={nome} estaNaLista={estaNaLista} />
+            <FunctionComponent nome={nome} tarefa={tarefa}></FunctionComponent>
+          </>
+        )
+       
+      })}
+      
+      {/* <ClassComponent nome="Nicolas" estaNaLista={true} />
       <ClassComponent nome="Pedro" estaNaLista={false} />
       <ClassComponent nome="Carolina" estaNaLista={true} />
       <h3>Tarefas: </h3>
       <FunctionComponent nome="Nicolas"  tarefa="batata-frita" />
       <FunctionComponent nome="Pedro"  tarefa="pizza" />
-      <FunctionComponent nome="Carolina"  tarefa="bebidas" />
+      <FunctionComponent nome="Carolina"  tarefa="bebidas" /> */}
     </div>
     
   );
